05962386fa1c638a263c3f370ad9bdddfb63e80d
Cameron Garnham <da2ce7@gmail.com>
2012-09-28 11:19:04 +1000

    java wrapper, first version

---
+++ src/main/java/com/wrapper/core/jni/App.java
@@ -0,0 +1,13 @@
+package com.wrapper.core.jni;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
